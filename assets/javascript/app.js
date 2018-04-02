44c2d1602b92ce281c902033dde9b6d138c0f724
Orit Freud <oritfreud@Orits-MacBook-Air.local>
2018-04-02 10:27:27 -0400

    fixed image upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -49,7 +49,7 @@ window.onload = function () {
 
     var fileInput = document.getElementById('fileInput');
     var fileDisplayArea = document.getElementById('fileDisplayArea');
-
+    var placeholderImage = document.getElementById('placeholderImage');
 
     fileInput.addEventListener('change', function (e) {
         var file = fileInput.files[0];
@@ -59,12 +59,12 @@ window.onload = function () {
             var reader = new FileReader();
 
             reader.onload = function (e) {
-                fileDisplayArea.innerHTML = "";
+                // fileDisplayArea.innerHTML = "";
+                placeholderImage.src = reader.result;
+                // var img = new Image();
+                // img.src = reader.result;
 
-                var img = new Image();
-                img.src = reader.result;
-
-                fileDisplayArea.appendChild(img);
+                // fileDisplayArea.appendChild(img);
 
             }
 
