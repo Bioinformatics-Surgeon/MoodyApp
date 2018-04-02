1f2f700f76a37b7430c12cfcbc057c8adfdc37d1
Caroline Rink <carolinerink@MacBook-Pro-2.local>
2018-04-02 11:06:27 -0400

    js updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,3 @@
-Spotify
-// alert("connected");
-
-
-
-
 $(document).ready(function() {
   
   var emotionMappingObject = {
@@ -45,9 +39,8 @@ $(document).ready(function() {
 // ** To grab the "Bearer-Token" from the redirected URL after logining into Spotify **
 // Example URL after the redirect:
 //https://bioinformatics-surgeon.github.io/MoodyApp/#access_token=BQBNFC7s_igIqfXCCQ2ALuqUorRjDTIdOe-B3jOj6OI-CO82EUYbXCMpK_Z0ZQqjAbPw4-uqXBplmWAnYNEc1MuKCHQZ6HukzAkMT7bvuhYWOaqM8mY_fYu4CvpcongtUtTki61eymrxBCw&token_type=Bearer&expires_in=3600
-// ======================================================================
-=======
-{ /* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> */ }
+// =============================================================================
+/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> */
 
 
 
